3b76cc5d7a497315d5ad57f0724ec7e1a550aa29
Sebastien Coutu <sebastien@metacommerce.app>
2023-07-12 11:52:57 -0400

    fix: initialize parameters should be an object and throw when check fail

--- src/Engage/Engage.test.ts
+++ src/Engage/Engage.test.ts
@@ -1,11 +1,12 @@
 import { Engage } from './Engage';
+import { MissingApiKeyMessage } from '../lib/errors';
 
 describe('Engage', () => {
   it('Should initialize the API key with the default URL', () => {
     const obj = new Engage();
     expect(obj).toBeInstanceOf(Engage);
     const apiKey = 'abcd-efgh-1234-5678';
-    obj.initialize(apiKey);
+    obj.initialize({ apiKey });
 
     expect(obj['apiKey']).toBeDefined();
     expect(obj['apiKey']).toBe(apiKey);
@@ -15,12 +16,22 @@ describe('Engage', () => {
     expect(obj['url']?.startsWith('https://')).toBe(true);
   });
 
+  it('Should throw when the SDK API key is empty', () => {
+    const obj = new Engage();
+    expect(obj).toBeInstanceOf(Engage);
+    const apiKey = '';
+    const t = () => {
+      obj.initialize({ apiKey });
+    };
+    expect(t).toThrowError(MissingApiKeyMessage);
+  });
+
   it('Should initialize the API key and URL', () => {
     const obj = new Engage();
     expect(obj).toBeInstanceOf(Engage);
     const apiKey = 'abcd-efgh-1234-5678';
     const newUrl = 'https://some.domain.app';
-    obj.initialize(apiKey, { url: newUrl });
+    obj.initialize({ apiKey, url: newUrl });
 
     expect(obj['apiKey']).toBeDefined();
     expect(obj['apiKey']).toBe(apiKey);
--- src/Engage/Engage.ts
+++ src/Engage/Engage.ts
@@ -1,11 +1,15 @@
 import { logger } from '../lib/logging';
+import { MissingApiKey } from '../lib/errors';
 
 export class Engage {
   private apiKey?: string;
   private url?: string;
 
-  initialize(apiKey: string, options?: { url?: string }): void {
-    this.apiKey = apiKey;
+  initialize(options: { apiKey: string; url?: string }): void {
+    if (!options.apiKey) {
+      throw new MissingApiKey();
+    }
+    this.apiKey = options.apiKey;
     const re = /(\w{3}).*/;
     logger.debug(`initialize: API Key starts with [ ${this.apiKey?.replace(re, '$1...........')} ]`);
     this.url = options?.url ?? 'https://rest.metacommerce.app';
