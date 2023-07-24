75b02cef9af71162da9a5e69382ee2a224c9b695
Maxime Antoine <maxime@antoinedev.com>
2023-07-24 14:23:32 -0400

    fix: add test + readme for custom event

--- src/Engage/Engage.test.ts
+++ src/Engage/Engage.test.ts
@@ -1,6 +1,13 @@
 import { Engage } from './Engage';
 import { MissingApiKeyMessage } from '../lib/errors';
 
+(global as any).fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({}),
+  }),
+);
+
 describe('Engage', () => {
   it('Should initialize the API key with the default URL', () => {
     const obj = new Engage();
@@ -48,5 +55,21 @@ describe('Engage', () => {
 
     expect(obj.events).toBeDefined();
     expect(obj.events).toHaveProperty('send');
+  });
+
+  it('should send a custom event', async () => {
+    const client = new Engage();
+
+    client.initialize({
+      apiKey: 'abcd-efgh-1234-5678',
+    });
+
+    await client.events.send({
+      type: 'test',
+      iHaveProps: 'yes',
+    });
+
+    expect((global as any).fetch).toHaveBeenCalledTimes(1);
+    expect((global as any).fetch).not.toThrow();
   });
 });
